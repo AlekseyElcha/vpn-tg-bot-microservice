d3fe781413d00169ce5112a4c0af9266e330cd6e
aleksey <aleshus2007@gmail.com>
2026-07-27 21:10:28 +0300

    updated server status info representation for users

--- src/handlers/status.py
+++ src/handlers/status.py
@@ -12,39 +12,77 @@ router = Router()
 async def process_balance_menu(callback_query: types.CallbackQuery):
     await callback_query.message.delete()
 
-    server_info_text = "<b>Статус сервиса:</b>\n\n"
+    api_ping_start = time.perf_counter()
+    api_ping_data = await api_client.ping_backend()
+    api_ping_finish = time.perf_counter()
 
-    server_info = await api_client.fetch_server_status()
+    if api_ping_data:
+        api_status = "доступен 🟢"
+    else:
+        api_status = "недоступен 🔴"
 
-    backend_ping_start = time.perf_counter()
+    api_ping_ms = int((api_ping_finish - api_ping_start) * 1000)
+    if api_ping_ms <= 250:
+        api_ping_ms = str(api_ping_ms)
+        api_ping_ms += " мс 🟢"
+    elif api_ping_ms <= 500:
+        api_ping_ms = str(api_ping_ms)
+        api_ping_ms += " мс 🟡"
+    elif api_ping_ms > 500 and  "недо" not in api_status:
+        api_ping_ms = str(api_ping_ms)
+        api_ping_ms += " мс 🟠"
+    else:
+        api_ping_ms = str(api_ping_ms)
+        api_status += " мс 🔴"
 
-    res = await api_client.ping_backend()
-    if res:
-        backend_ping_finish = time.perf_counter()
+    vpn_server_ping = await api_client.ping_vpn_server()
+    server_data = await api_client.fetch_server_status()
 
-        ping_time_ms = int((backend_ping_finish - backend_ping_start) * 1000)
+    server_status = ""
+    xray_status = ""
+    admin_comment = ""
 
-        if ping_time_ms <= 50:
-            api_status_text = f"⚡ {ping_time_ms} мс"
-        elif ping_time_ms <= 150:
-            api_status_text = f"🟢 {ping_time_ms} мс"
+    if server_data.get("server_running"):
+        server_status = "доступен 🟢"
+    else:
+        server_status = "недоступен 🔴"
+
+    if server_data.get("xray_running"):
+        xray_status = "запущен 🟢"
+    else:
+        xray_status = "не запущен 🔴"
+
+    if server_data.get("comment"):
+        admin_comment = server_data.get("comment")
+    else:
+        admin_comment = "нет информации 🔸"
+
+    if vpn_server_ping and vpn_server_ping.get("success") == True:
+        vpn_ping_ms = vpn_server_ping.get("ping")
+        if vpn_ping_ms <= 80:
+            vpn_ping_ms = str(vpn_ping_ms)
+            vpn_ping_ms += " мс 🟢"
+        elif vpn_ping_ms <= 250:
+            vpn_ping_ms = str(vpn_ping_ms)
+            vpn_ping_ms += " мс 🟡"
+        elif vpn_ping_ms <= 500 and "недо" not in vpn_ping_ms:
+            vpn_ping_ms = str(vpn_ping_ms)
+            vpn_ping_ms += " мс 🟠"
         else:
-            api_status_text = f"🟡 {ping_time_ms} мс"
+            vpn_ping_ms = str(vpn_ping_ms)
+            vpn_ping_ms += " мс 🔴"
     else:
-        api_status_text = "❌ Недоступен (Time Out)"
+        vpn_ping_ms = "no info 🔸"
 
 
-    if server_info is None:
-        server_info_text += "Не удалось получить информацию..."
-    else:
-        server_running = "Работает 🟢" if server_info.get("server_running") else "Не работает 🔴"
-        xray_running = "Работает 🟢" if server_info.get("xray_running") else "Не работает 🔴"
-        comment = server_info.get("comment", "Нет комментариев")
+    server_info_text = ("<b>• Статус сервиса:</b>\n\n"
+                       f"<b>• VPN-сервер: {server_status}</b>\n"
+                        f"└── ping: {vpn_ping_ms}\n\n"
+                        f"<b>• Xray: {xray_status}</b>\n\n"
+                        f"<b>• API: {api_status}</b>\n"
+                        f"└── ping: {api_ping_ms}\n\n"
+                        f"<b>• Комментарий: {admin_comment}</b>")
 
-        server_info_text += f"<b>VPN-cервер:</b> {server_running}\n"
-        server_info_text += f"<b>API:</b> {api_status_text}\n"
-        server_info_text += f"<b>Xray:</b> {xray_running}\n"
-        server_info_text += f"<b>Комментарий:</b> {comment}"
 
     await callback_query.message.answer(
         text=server_info_text,
