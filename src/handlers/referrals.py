314f99990664d5b8ba66c77ed7f990678c7792cd
aleksey <aleshus2007@gmail.com>
2026-07-29 11:04:19 +0300

    added referrals aiogram router with instructions & referral link

---
+++ src/handlers/referrals.py
@@ -0,0 +1,36 @@
+from aiogram import Router, types
+from aiogram.exceptions import TelegramBadRequest
+
+from src.keyboards.menu import get_main_menu_keyboard
+from src.services.backend_api import api_client
+
+router = Router()
+
+@router.callback_query(lambda c: c.data == "referrals_btn_click")
+async def process_referrals_btn_click(
+        callback_query: types.CallbackQuery
+):
+    try:
+        ref_link_req_resp = await api_client.fetch_referral_link(tg_id=callback_query.from_user.id)
+        if not ref_link_req_resp or not ref_link_req_resp.get("success"):
+            await callback_query.message.edit_text(
+                text=f"Произошла ошибка!",
+                reply_markup=get_main_menu_keyboard()
+            )
+
+        ref_link = ref_link_req_resp.get("referral_link")
+
+        instruction_with_link = (
+            f"<b>Представляем реферальную систему УруруVPN!</b>\n\n"
+            f"Делитесь своей ссылкой, за каждого приведённого друга Вы и Ваш друг получат бонусный баланс!\n\n"
+            f"Ваша ссылка: <code>{ref_link}</code>"
+        )
+
+        await callback_query.message.edit_text(
+            text=instruction_with_link,
+            reply_markup=get_main_menu_keyboard()
+        )
+
+        await callback_query.answer()
+    except TelegramBadRequest:
+        pass
