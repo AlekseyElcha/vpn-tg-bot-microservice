b2679b562a7b06717962394d3915a2ef9a03a270
aleksey <aleshus2007@gmail.com>
2026-07-31 16:50:27 +0300

    updated vpn config

--- src/config/vpn.py
+++ src/config/vpn.py
@@ -1,11 +1,6 @@
-import json
-import os
-
-from dotenv import load_dotenv
 from pydantic import BaseModel
 
 
-load_dotenv()
 class VpnConfig(BaseModel):
-    inbounds: list[int] = json.loads(os.getenv("VPN_INBOUNDS", "[6,7,8]"))
-    limit_ip: int = os.getenv("VPN_LIMIT_IP")
+    inbounds: list[int]
+    limit_ip: int
