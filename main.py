c05bda77f01aa6dd032a5a43c8fad78ca89122da
alesh <alesh@LAPTOP-SWORD17>
2026-08-02 21:43:16 +0300

    updated tg bot startup & shutdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 
+from aiocryptopay import AioCryptoPay, Networks
 from aiogram import Dispatcher
 from redis.asyncio import Redis
 
@@ -7,6 +8,17 @@ from src.handlers import main_router
 from src.loader import bot, dp
 from src.logs import setup_logging, logger
 from src.rmq_client.consumer import start_consuming
+
+
+CRYPTO_TOKEN = "" # TODO MOVE TO env + config!!!!!
+
+_crypto_client = None
+
+def get_crypto() -> AioCryptoPay:
+    global _crypto_client
+    if _crypto_client is None:
+        _crypto_client = AioCryptoPay(token=CRYPTO_TOKEN, network=Networks.TEST_NET)
+    return _crypto_client
 
 
 async def on_startup(dispatcher: Dispatcher):
@@ -17,6 +29,10 @@ async def on_startup(dispatcher: Dispatcher):
 async def on_shutdown(dispatcher: Dispatcher):
     logger.info("Stopping background tasks...")
     consuming_task = dispatcher.get("consuming_task")
+    crypto_client: AioCryptoPay = dp.get("crypto")
+    if crypto_client:
+        await crypto_client.close()
+        logger.info("CryptoPay session closed")
     if consuming_task and not consuming_task.done():
         consuming_task.cancel()
         try:
@@ -29,8 +45,9 @@ async def on_shutdown(dispatcher: Dispatcher):
 async def main():
     dp.include_router(main_router)
     redis_pool = Redis.from_url("redis://localhost:6379", decode_responses=True)
+    crypto_client = AioCryptoPay(token=CRYPTO_TOKEN, network=Networks.TEST_NET)
     dp["redis"] = redis_pool
-
+    dp["crypto"] = crypto_client
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
 
