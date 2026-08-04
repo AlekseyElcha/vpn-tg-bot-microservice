85714ffe45e5bfd3fc0aa31ca61e61a8df233f43
alesh <alesh@LAPTOP-SWORD17>
2026-08-04 18:54:14 +0300

    created rmq_connection_url property function in RabbitMQConfig

--- src/config/rabbitmq.py
+++ src/config/rabbitmq.py
@@ -6,3 +6,7 @@ class RabbitMQConfig(BaseModel):
     port: int
     user: str
     password: str
+
+    @property
+    def rmq_connection_url(self):
+        return f"amqp://{self.user}:{self.password}@{self.host}:{self.port}/%2f"
