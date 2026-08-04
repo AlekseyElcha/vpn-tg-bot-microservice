91940b269d2fe0e4a06c9c61280ea14278f6ebdd
alesh <alesh@LAPTOP-SWORD17>
2026-08-04 18:52:23 +0300

    new logic of menus for crypto + massive work done...

--- src/handlers/crypto_payment.py
+++ src/handlers/crypto_payment.py
@@ -1,9 +1,10 @@
 from aiocryptopay import AioCryptoPay , Networks
+from aiocryptopay.exceptions import CryptoPayAPIError
 from aiogram import Router, F, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from src.keyboards.menu import get_balance_keyboard
+from src.keyboards.menu import get_balance_keyboard, get_main_menu_keyboard
 from src.loader import bot
 from src.services.backend_api import api_client
 from src.services.currency_exchange import exchange_crypto_to_tg_stars
@@ -14,6 +15,85 @@ router = Router()
 @router.callback_query(lambda c: c.data == "add_money_crypto_btn_click")
 async def process_add_money_btn_click(
         callback_query: types.CallbackQuery
+):
+    builder = InlineKeyboardBuilder()
+
+    builder.row(
+        types.InlineKeyboardButton(
+            text=f"GRAM (ex. Toncoin) 💎",
+            callback_data=f"choose_crypto_ton"
+        )
+    )
+    builder.row(
+        types.InlineKeyboardButton(
+            text=f"Tether (USDT) 💰",
+            callback_data=f"choose_crypto_usdt"
+        )
+    )
+    builder.row(
+        types.InlineKeyboardButton(
+            text=f"TRON (TRX) [тест получения из Redis]",
+            callback_data=f"choose_crypto_trx"
+        )
+    )
+    builder.row(
+        types.InlineKeyboardButton(
+            text=f"⬅️ Назад",
+            callback_data=f"my_balance_btn_click"
+        )
+    )
+    await callback_query.message.edit_text(
+        text=f"<b>Выберите криптовалюту:</b>",
+        reply_markup=builder.as_markup()
+    )
+
+    await callback_query.answer()
+
+
+
+# @router.callback_query(lambda c: c.data == "add_money_crypto_btn_click")
+# async def process_add_money_btn_click(
+#         callback_query: types.CallbackQuery
+# ):
+#     builder = InlineKeyboardBuilder()
+#
+#     builder.row(
+#         types.InlineKeyboardButton(
+#             text=f"1 TON ~ 100🌟",
+#             callback_data=f"add_crypto_0.1_TON"
+#         )
+#     )
+#     builder.row(
+#         types.InlineKeyboardButton(
+#             text=f"2 TON ~ 200🌟",
+#             callback_data=f"add_crypto_0.2_TON"
+#         )
+#     )
+#     builder.row(
+#         types.InlineKeyboardButton(
+#             text=f"3 TON ~ 300🌟",
+#             callback_data=f"add_crypto_0.3_TON"
+#         )
+#     )
+#
+#     builder.row(
+#         types.InlineKeyboardButton(
+#             text=f"⬅️ Назад",
+#             callback_data=f"my_balance_btn_click"
+#         )
+#     )
+#
+#     await callback_query.message.edit_text(
+#         text=f"Выберите сумму для пополнения:",
+#         reply_markup=builder.as_markup()
+#     )
+#
+#     await callback_query.answer()
+
+
+@router.callback_query(F.data.startswith("choose_crypto_ton"))
+async def process_pay_crypto_btn_click(
+        callback_query: types.CallbackQuery,
 ):
     builder = InlineKeyboardBuilder()
 
@@ -39,7 +119,7 @@ async def process_add_money_btn_click(
     builder.row(
         types.InlineKeyboardButton(
             text=f"⬅️ Назад",
-            callback_data=f"my_balance_btn_click"
+            callback_data=f"add_money_crypto_btn_click"
         )
     )
 
@@ -51,17 +131,114 @@ async def process_add_money_btn_click(
     await callback_query.answer()
 
 
+@router.callback_query(F.data.startswith("choose_crypto_usdt"))
+async def process_pay_crypto_btn_click(
+        callback_query: types.CallbackQuery,
+):
+    ratio = await api_client.fetch_crypto_currency_ratio(
+        currency_code="USDT"
+    )
+
+
+    builder = InlineKeyboardBuilder()
+
+    builder.row(
+        types.InlineKeyboardButton(
+            text=f"1 USDT ~ {ratio * 1}🌟",
+            callback_data=f"add_crypto_1_USDT"
+        )
+    )
+    builder.row(
+        types.InlineKeyboardButton(
+            text=f"2 USDT ~ {ratio * 2}🌟",
+            callback_data=f"add_crypto_2_USDT"
+        )
+    )
+    builder.row(
+        types.InlineKeyboardButton(
+            text=f"3 USDT ~ {ratio * 3}🌟",
+            callback_data=f"add_crypto_3_USDT"
+        )
+    )
+
+    builder.row(
+        types.InlineKeyboardButton(
+            text=f"⬅️ Назад",
+            callback_data=f"add_money_crypto_btn_click"
+        )
+    )
+
+    await callback_query.message.edit_text(
+        text=f"Выберите сумму для пополнения:",
+        reply_markup=builder.as_markup()
+    )
+
+    await callback_query.answer()
+
+
+@router.callback_query(F.data.startswith("choose_crypto_trx"))
+async def process_pay_crypto_btn_click(
+        callback_query: types.CallbackQuery,
+):
+    ratio = await api_client.fetch_crypto_currency_ratio_v2(
+        currency_code="TRX"
+    )
+
+    if not ratio:
+        await callback_query.message.edit_text(
+            text=f"Курсы криптовалют обновляются, повторите попытку позже...",
+            reply_markup=get_balance_keyboard()
+        )
+
+    builder = InlineKeyboardBuilder()
+
+    builder.row(
+        types.InlineKeyboardButton(
+            text=f"3 TRX ~ {ratio * 3}🌟",
+            callback_data=f"add_crypto_3_TRX"
+        )
+    )
+    builder.row(
+        types.InlineKeyboardButton(
+            text=f"6 TRX ~ {ratio * 6}🌟",
+            callback_data=f"add_crypto_6_TRX"
+        )
+    )
+    builder.row(
+        types.InlineKeyboardButton(
+            text=f"9 TRX ~ {ratio * 9}🌟",
+            callback_data=f"add_crypto_9_TRX"
+        )
+    )
+
+    builder.row(
+        types.InlineKeyboardButton(
+            text=f"⬅️ Назад",
+            callback_data=f"add_money_crypto_btn_click"
+        )
+    )
+
+    await callback_query.message.edit_text(
+        text=f"Выберите сумму для пополнения:",
+        reply_markup=builder.as_markup()
+    )
+
+    await callback_query.answer()
+
+
+
 @router.callback_query(F.data.startswith("add_crypto_"))
 async def process_pay_crypto_btn_click(
         callback_query: types.CallbackQuery,
         crypto: AioCryptoPay
 ):
     amount = float(callback_query.data.split("_")[2])
+    asset = callback_query.data.split("_")[3]
 
     payment_invoice = await crypto.create_invoice(
-        asset='TON',
+        asset=asset,
         amount=amount,
-        description=f'Пополнение баланса УруруVPN - {amount} TON'
+        description=f'Пополнение баланса УруруVPN - {amount}'
     )
 
     pay_kb = InlineKeyboardMarkup(inline_keyboard=[
@@ -80,106 +257,147 @@ async def process_pay_crypto_btn_click(
     await callback_query.answer()
 
 
-@router.callback_query(F.data.startswith("check_"))
-async def check_payment_status(
-        callback_query: types.CallbackQuery,
-        crypto: AioCryptoPay
-):
-    invoice_id = int(callback_query.data.split("_")[1])
-    tariff_name = callback_query.data.split("_")[1]
+# @router.callback_query(F.data.startswith("check_"))
+# async def check_payment_status(
+#         callback_query: types.CallbackQuery,
+#         crypto: AioCryptoPay
+# ):
+#     invoice_id = int(callback_query.data.split("_")[1])
+#
+#     invoices = await crypto.get_invoices(invoice_ids=invoice_id)
+#
+#     invoice = invoices[0] if isinstance(invoices, list) else invoices
+#
+#     if invoice.status == 'paid':
+#         await callback_query.message.edit_reply_markup(reply_markup=None)
+#
+#         await callback_query.answer("🎉 Оплата прошла успешно!", show_alert=True)
+#
+#         payment_amount = await exchange_crypto_to_tg_stars(
+#             crypto_name=invoice.asset,
+#             crypto_amount=invoice.amount
+#         )
+#
+#         if not payment_amount:
+#             spend_id = f"refund_inv_{invoice.invoice_id}"
+#             await crypto.transfer(
+#                 user_id=callback_query.from_user.id,
+#                 amount=invoice.amount,
+#                 asset=invoice.asset,
+#                 spend_id=spend_id
+#             )
+#
+#
+#         server_response = await api_client.fetch_server_post_payment_task_result(
+#             user_id=callback_query.from_user.id,
+#             item_id=f"{invoice.amount} {invoice.asset}",
+#             payment_amount=payment_amount
+#         )
+#
+#         if server_response is None:
+#             await crypto.transfer(
+#                 user_id=callback_query.from_user.id,
+#                 amount=invoice.amount,
+#                 asset=invoice.asset,
+#                 spend_id=f"refund_inv_{invoice.invoice_id}"
+#             )
+#             await bot.send_message(
+#                 chat_id=int(callback_query.from_user.id),
+#                 text="Что-то пошло не так, поэтому мы вернули Ваши средства.",
+#                 reply_markup=get_balance_keyboard()
+#             )
+#
+#         server_task_successful = server_response.get("success")
+#
+#         if not server_task_successful or server_task_successful == False:
+#             await crypto.transfer(
+#                 user_id=callback_query.from_user.id,
+#                 amount=invoice.amount,
+#                 asset=invoice.asset,
+#                 spend_id=f"refund_inv_{invoice.invoice_id}"
+#             )
+#             await bot.send_message(
+#                 chat_id=int(callback_query.from_user.id),
+#                 text="Произошла ошибка, поэтому мы вернули Ваши средства.",
+#                 reply_markup=get_balance_keyboard()
+#             )
+#
+#         payment_test_mode_enabled = True
+#         if payment_test_mode_enabled:
+#             try:
+#                 await crypto.transfer(
+#                     user_id=callback_query.from_user.id,
+#                     amount=invoice.amount,
+#                     asset=invoice.asset,
+#                     spend_id=f"refund_inv_{invoice.invoice_id}"
+#                 )
+#             except Exception as e:
+#                 print(f"Не удалось выполнить автовозврат: {e}")
+#
+#         await bot.send_message(
+#             chat_id=int(callback_query.from_user.id),
+#             text=f"<b>Ура! 🎉\n\n"
+#                  f"На баланс Вашего аккаунта успешно зачислено {payment_amount}🌟\n\n"
+#                  f"Благодарим за пользование услугами УруруVPN!</b>",
+#             reply_markup=get_balance_keyboard(),
+#             message_effect_id="5046509860389126442"
+#         )
 
-    invoices = await crypto.get_invoices(invoice_ids=invoice_id)
+    @router.callback_query(F.data.startswith("check_"))
+    async def check_payment_status(
+            callback_query: types.CallbackQuery,
+            crypto: AioCryptoPay
+    ):
+        invoice_id = int(callback_query.data.split("_")[1])
 
-    if not invoices:
-        await callback_query.answer("Счет не найден.", show_alert=True)
-        return
+        invoices = await crypto.get_invoices(invoice_ids=invoice_id)
 
-    current_invoice = invoices
+        if not invoices:
+            await callback_query.answer("Счет не найден.", show_alert=True)
+            return
 
-    user_id = callback_query.from_user.id
-    if isinstance(invoices, list):
-        invoice = invoices[0] if invoices else None
-    else:
-        invoice = invoices
+        invoice = invoices[0] if isinstance(invoices, list) else invoices
 
-    if current_invoice.status == 'paid':
-        await callback_query.message.edit_reply_markup(reply_markup=None)
+        if invoice.status == 'paid':
+            await callback_query.message.edit_reply_markup(reply_markup=None)
+            await callback_query.answer("🎉 Оплата прошла успешно!", show_alert=True)
 
-        await callback_query.answer("🎉 Оплата прошла успешно!", show_alert=True)
-
-        payment_amount = exchange_crypto_to_tg_stars(
-            crypto_name=invoice.asset,
-            crypto_amount=invoice.amount
-        )
-
-        if not payment_amount:
-            await crypto.transfer(
-                user_id=user_id,
-                amount=invoice.amount,
-                asset=invoice.asset,
-                spend_id=f"refund_inv_{invoice.invoice_id}"
+            payment_amount = await exchange_crypto_to_tg_stars(
+                crypto_name=invoice.asset,
+                crypto_amount=invoice.amount
             )
 
-
-        server_response = await api_client.fetch_server_post_payment_task_result(
-            user_id=user_id,
-            item_id=f"{invoice.amount} {invoice.asset}",
-            payment_amount=payment_amount
-        )
-
-        if server_response is None:
-            await crypto.transfer(
-                user_id=user_id,
-                amount=invoice.amount,
-                asset=invoice.asset,
-                spend_id=f"refund_inv_{invoice.invoice_id}"
-            )
-            await bot.send_message(
-                chat_id=int(user_id),
-                text="Что-то пошло не так, поэтому мы вернули Ваши средства.",
-                reply_markup=get_balance_keyboard()
+            server_response = await api_client.fetch_server_post_payment_task_result(
+                            user_id=callback_query.from_user.id,
+                            item_id=f"{invoice.amount} {invoice.asset}",
+                            payment_amount=payment_amount
             )
 
-        server_task_successful = server_response.get("success")
+            server_task_successful = server_response.get("success")
 
-        if not server_task_successful or server_task_successful == False:
-            await crypto.transfer(
-                user_id=user_id,
-                amount=invoice.amount,
-                asset=invoice.asset,
-                spend_id=f"refund_inv_{invoice.invoice_id}"
-            )
-            await bot.send_message(
-                chat_id=int(user_id),
-                text="Произошла ошибка, поэтому мы вернули Ваши средства.",
-                reply_markup=get_balance_keyboard()
-            )
-
-        payment_test_mode_enabled = True
-        if payment_test_mode_enabled:
-            try:
+            if not server_task_successful or server_task_successful == False:
                 await crypto.transfer(
-                    user_id=user_id,
+                    user_id=callback_query.from_user.id,
                     amount=invoice.amount,
                     asset=invoice.asset,
                     spend_id=f"refund_inv_{invoice.invoice_id}"
                 )
-            except Exception as e:
-                print(f"Не удалось выполнить автовозврат: {e}")
+                await bot.send_message(
+                    chat_id=int(callback_query.from_user.id),
+                    text="Произошла ошибка, поэтому мы вернули Ваши средства.",
+                    reply_markup=get_balance_keyboard()
+                )
+            else:
+                await callback_query.message.answer(
+                    text=f"Баланс успешно пополнен на {int(payment_amount)}🌟!",
+                    message_effect_id="5046509860389126442",
+                    reply_markup=get_main_menu_keyboard()
+                )
 
-        await bot.send_message(
-            chat_id=int(user_id),
-            text=f"<b>Ура! 🎉\n\n"
-                 f"На баланс Вашего аккаунта успешно зачислено {payment_amount}🌟\n\n"
-                 f"Благодарим за пользование услугами УруруVPN!</b>",
-            reply_markup=get_balance_keyboard(),
-            message_effect_id="5046509860389126442"
-        )
 
+        elif invoice.status == 'active':
+            await callback_query.answer("⏳ Оплата пока не обнаружена. Попробуйте еще раз через пару секунд.", show_alert=True)
 
-    elif current_invoice.status == 'active':
-        await callback_query.answer("⏳ Оплата пока не обнаружена. Попробуйте еще раз через пару секунд.", show_alert=True)
-
-    elif current_invoice.status == 'expired':
-        await callback_query.message.edit_reply_markup(reply_markup=None)
-        await callback_query.answer("❌ Время действия счета истекло. Создайте новый.", show_alert=True)
+        elif invoice.status == 'expired':
+            await callback_query.message.edit_reply_markup(reply_markup=None)
+            await callback_query.answer("❌ Время действия счета истекло. Создайте новый.", show_alert=True)
